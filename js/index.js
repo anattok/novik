ba4acbc8f274b0b3b7750db1bbcefa4898312bf2
Anatoliy Tokarev <tokarev182@yandex.ru>
2023-04-11 22:47:36 +0300

    burger menu

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// burger menu
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle("menu--active");
+    burger.classList.toggle("burger--active");
+})
